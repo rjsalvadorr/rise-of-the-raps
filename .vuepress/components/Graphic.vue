d4cbac75d3a97169e49ae1c1725f290c6bf1708c
RJ Salvador <randolph.salvador@gmail.com>
2019-06-24 11:05:53 -0400

    Added left/right scroll areas to graphic

--- .vuepress/components/Graphic.vue
+++ .vuepress/components/Graphic.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="graphic-wrapper">
     <p>D3 graphic placeholder</p>
-    <div class="scroll-btn scroll-btn--left">
-      <span>⇦</span>
+    <div class="scroll-btn scroll-btn--left" v-on:click="scrollPrev">
+      <span class="scroll-btn-icon">⇦</span>
     </div>
-    <div class="scroll-btn scroll-btn--right">
-      <span>⇨</span>
+    <div class="scroll-btn scroll-btn--right" v-on:click="scrollNext">
+      <span class="scroll-btn-icon">⇨</span>
     </div>
   </div>
 </template>
@@ -13,6 +13,26 @@
 <script>
 export default {
   name: 'Graphic',
+  data: function() {
+    return {
+      currentSlide: 1,
+      currentYear: 2019,
+    }
+  },
+  methods: {
+    scrollPrev: function(evt) {
+      if(this.currentSlide > 1) {
+        this.currentSlide = this.currentSlide - 1;
+      }
+      console.log(`currentSlide: ${this.currentSlide}`);
+    },
+    scrollNext: function(evt) {
+      if(this.currentSlide < 25) {
+        this.currentSlide = this.currentSlide + 1;
+      }
+      console.log(`currentSlide: ${this.currentSlide}`);
+    }
+  },
 }
 </script>
 
@@ -20,20 +40,48 @@ export default {
   @import "../styles/vars.scss";
 
   .graphic-wrapper {
-    background-color: #FF0000;
+    position: relative;
+    background-color: #111111;
     min-height: 400px;
+  }
+
+  .scroll-btn {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 15%;
+    cursor: pointer;
+    z-index: 11;
+    transition: background-color 0.25s;
+
+    &--left {
+      left: 0;
+    }
+
+    &--right {
+      right: 0;
+    }
+
+    &-icon {
+      position: relative;
+      color: #FFFFFF;
+      z-index: 12;
+    }
+
+    &:hover {
+      background-color: rgba(64, 64, 64, 0.25);
+    }
   }
 
   /* Larger than tablet */
   @media (min-width: 750px) {
     .graphic-wrapper {
       position: fixed;
-      width: 70%;
+      width: calc(70% - 40px);
       left: 0;
       top: 0;
       bottom: 0;
-      padding: $space-unit;
-      z-index: -10;
+      z-index: 10;
     }
   }
 </style>
